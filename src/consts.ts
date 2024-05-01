6f20d1dfb49811e00f9a149df65abb0298586fb4
Brian Weber <brian.g.weber@gmail.com>
2024-04-30 23:45:06 -0400

    Poke

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Brian G Weber Photography';
-export const SITE_DESCRIPTION = 'Welcome to my blog! Check out the store for prints!';
+export const SITE_DESCRIPTION = 'Welcome to my blog! Check out the store for prints on a variety of products!';
 export const MASTODON_URL = "https://astrodon.social/@webcubus";
 export const FACEBOOK_URL = "https://www.facebook.com/briangweberphotography";
 export const INSTAGRAM_URL = "https://www.instagram.com/briangweber/";
