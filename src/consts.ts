d77734c0aade508a42372be7551256cbeae7a2c3
Brian Weber <brian.g.weber@gmail.com>
2024-04-30 23:29:38 -0400

    Initial commit!

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Brian G Weber Photography';
+export const SITE_DESCRIPTION = 'Welcome to my blog! Check out the store for prints!';
+export const MASTODON_URL = "https://astrodon.social/@webcubus";
+export const FACEBOOK_URL = "https://www.facebook.com/briangweberphotography";
+export const INSTAGRAM_URL = "https://www.instagram.com/briangweber/";
+export const TWITTER_URL = "https://twitter.com/briangweber";
+export const YOUTUBE_URL = "https://www.youtube.com/channel/UCvnKTR6htKtOmJAIZ-YW8Pw";
