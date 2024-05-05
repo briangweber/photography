2a1cf4925062d00dccc4db3eaadaa699afbb8855
Brian Weber <brian.g.weber@gmail.com>
2024-05-05 14:48:14 -0400

    Swap back to see if GitHub pages will work

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.briangweber.com',
+	site: 'https://briangweber.github.io',
 	integrations: [mdx(), sitemap()],
 });
