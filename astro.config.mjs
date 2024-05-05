01d0d6a6511516258bc83e94d28516b9fe19ada0
Brian Weber <brian.g.weber@gmail.com>
2024-05-05 10:32:20 -0400

    Try amplify plugin again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.briangweber.com',
+	// site: 'https://blog.briangweber.com',
 	integrations: [mdx(), sitemap()],
-	base: "/"
+	base: "/",
+	output: "server", 
+  adapter: awsAmplify(), 
 });
