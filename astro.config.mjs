bdc30c327f6aecb60f2b2f6dbf9480002787d3f7
Brian Weber <brian.g.weber@gmail.com>
2024-05-05 10:38:20 -0400

    Revert "Try reverting Amplify config"
    This reverts commit 7cdd30f0aa175cda82393aee29bab1e5bfb197e3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import awsAmplify from 'astro-aws-amplify'; 
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.briangweber.com',
+	output: 'server',
 	integrations: [mdx(), sitemap()],
-	base: "/"
+	adapter: awsAmplify(), 
 });
