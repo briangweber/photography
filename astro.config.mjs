1caccaa876b90e972c4291675eff35e3da2270d4
Brian Weber <brian.g.weber@gmail.com>
2024-05-05 13:09:50 -0400

    Revert "Try changing config since GitHub pages now has a custom domain"
    This reverts commit b2077295d0ae3dc991a006e7ab24cd4ff5ee16c0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.briangweber.com',
+	site: 'https://briangweber.github.io',
 	integrations: [mdx(), sitemap()],
-	base: "/"
+	base: "/photography"
 });
